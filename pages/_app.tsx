80607d06caf56b86fea9948aa8399d88497d1781
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2023-11-08 11:06:06 +0100

    Add viem, wagmi and configure app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WagmiConfig, configureChains, createConfig, mainnet } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [mainnet],
+    [publicProvider()]
+  );
+  const config = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient
+  });
+  return (
+    <WagmiConfig config={config}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
